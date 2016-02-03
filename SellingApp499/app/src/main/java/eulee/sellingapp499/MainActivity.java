6c2acc39670c5e951abf96f302df2498dc656420
eulee0 <eugene.lee095@gmail.com>
2016-02-02 21:49:26 -0800

    Restart from scratch, made more activities
    Activities past Main acting up

---
+++ SellingApp499/app/src/main/java/eulee/sellingapp499/MainActivity.java
@@ -0,0 +1,115 @@
+package eulee.sellingapp499;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.fragment_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
+
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        Toast.makeText(this, "Menu item selected -> " + position, Toast.LENGTH_SHORT).show();
+
+        switch (position) {
+            case 1:
+                Intent intent1 = new Intent(this, OfferViewActivity.class);
+                startActivity(intent1);
+            case 2:
+                Intent intent2 = new Intent(this,MainActivity.class);
+                startActivity(intent2);
+            case 3:
+                Intent intent3 = new Intent(this, SettingsActivity.class);
+                startActivity(intent3);
+            case 4:
+                Intent intent4 = new Intent(this, LoginActivity.class);
+                startActivity(intent4);
+            case 5:
+                Intent intent5 = new Intent(this, CreateAccActivity.class);
+                startActivity(intent5);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mNavigationDrawerFragment.isDrawerOpen())
+            mNavigationDrawerFragment.closeDrawer();
+        else
+            super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_message) {
+            return true;
+        }
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickedMessage(MenuItem item) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        startActivity(intent);
+    }
+}
+
---
+++ SellingApp499/app/src/main/java/eulee/sellingapp499/NavigationDrawerCallbacks.java
@@ -0,0 +1,9 @@
+package eulee.sellingapp499;
+
+import android.support.v4.app.Fragment;
+
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+
+
+}
