e23401edd9fb28a75d6ef5ddd1f166c5a477ae0d
eulee0 <eugene.lee095@gmail.com>
2016-03-06 18:44:40 -0800

    Deleted older files

--- SellingApp499/app/src/main/java/eulee/sellingapp499/NavDrawer/NavigationDrawerCallbacks.java
+++
@@ -1,9 +0,0 @@
-package eulee.sellingapp499.NavDrawer;
-
-import android.support.v4.app.Fragment;
-
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-
-
-}
--- SellingApp499/app/src/main/java/eulee/sellingapp499/NavigationDrawerCallbacks.java
+++
@@ -1,9 +0,0 @@
-package eulee.sellingapp499;
-
-import android.support.v4.app.Fragment;
-
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-
-
-}
